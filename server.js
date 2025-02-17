34208ba0e499f09bde21313e5024759bbcd16025
kishanDCS <167978929+kishanDCS@users.noreply.github.com>
2025-02-17 05:55:43 +0000

    8

---
+++ server.js
@@ -0,0 +1,15 @@
+const { networkInterfaces } = require('os');
+
+function getMacAddress() {
+    const nets = networkInterfaces();
+    for (const interface in nets) {
+        for (const net of nets[interface]) {
+            if (!net.internal && net.mac !== '00:00:00:00:00:00') {
+                return net.mac;
+            }
+        }
+    }
+}
+
+console.log(getMacAddress());
+
